51c63e9d1dad97741e22973e1d2f889fd74a3a39
Raul Faife <35008453+RaulFaife@users.noreply.github.com>
2023-03-09 19:10:15 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# FabricEngine
+ New codebase for fabric engine
---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+const server = require('http').createServer(app)
+const io = require('socket.io')(server, { cors: { origin: "*" }})
+const port = 3001
+
+app.use(express.static('public'))
+
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
+app.listen(port, () => {
+    console.log("server is running...")
+})
+
+io.on("connection", (socket) => {
+    console.log("User connected: " + socket.id)
+})
